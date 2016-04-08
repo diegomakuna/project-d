971e834ec9b3e68834530f020eedcb7551006cc7
unknown <diego.araujo@NB-8634.office.netbiis.com>
2016-04-08 16:18:01 -0300

    Add gulp file and packge json

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+﻿var gulp = require('gulp'),
+    stylus = require('gulp-stylus'),
+    minifyCSS = require('gulp-minify-css'),
+    nib = require('nib'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync'),
+    notify = require("gulp-notify");
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+var paths = {
+    scripts: [
+        './js/jquery-1.12.2.js',
+        './js/main.js'
+    ],
+    css: 'css/**/*.styl',
+    html: '*.html'
+};
+
+gulp.task('stylus', function () {
+    gulp.src('./css/styl/main.styl')
+        .pipe(stylus({ error: true, use: [nib()] }))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./css'))
+        .pipe(notify("CSS OK!"))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src(paths.scripts)
+        .pipe(concat('./main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'))
+        .pipe(notify("JS OK!"));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.css, ['stylus', browserSync.reload]);
+    gulp.watch(paths.scripts, ['scripts', browserSync.reload]);
+    gulp.watch(paths.html, ['', browserSync.reload]);
+});
+
+gulp.task('default', ['watch', 'stylus', 'scripts', 'browser-sync']);
