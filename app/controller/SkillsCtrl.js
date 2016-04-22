f1914595e6738730ef00afaf2104d7214d0409bf
unknown <diego.araujo@NB-8634.office.netbiis.com>
2016-04-22 16:23:37 -0300

    Options knob

--- app/controller/SkillsCtrl.js
+++ app/controller/SkillsCtrl.js
@@ -1,8 +1,13 @@
 var SkillController = function($scope){
         $scope.value = 65;
         $scope.options = {
-        size: 100
-        //other options
+            displayPrevious: true,
+            barCap: 25,
+            trackWidth: 30,
+            barWidth: 20,
+            trackColor: 'rgba(255,0,0,.1)',
+            prevBarColor: 'rgba(0,0,0,.2)',
+            textColor: 'rgba(255,0,0,.6)'
         };
 };
 app.controller('SkillsCtrl', SkillController );
