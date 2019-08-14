bd303bad5e4c055ccb0ddee75f0db4d1c855cd06
diego.araujo <diego.araujo@netbiis.com>
2019-08-14 01:15:39 -0300

    change to react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/shared/sidebar'
+
+
+
+function App() {
+  return (
+    <div id="colorlib-page">
+    <div id="container-wrap">
+       <Sidebar></Sidebar>
+    <div id="colorlib-main">
+            teste
+        </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
