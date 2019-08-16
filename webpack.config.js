9cc800bfe62f85f43408febd1cf9fc9ecdd58be2
Diego Araujo <diego.araujo@netbiis.com>
2019-08-16 13:03:54 -0300

    configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,60 @@
-const HtmlSWebPackPlugin = require('html-webpack-plugin')
-
-const HtmlPlugin = new HtmlSWebPackPlugin({
-    template: './src/index.html',
-    filename: './index.html'
-})
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    module:{
-        rules:[
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: "babel-loader"
-                }
-
-            }
+  entry:'./src/index.js',
+  output:{
+      path:path.join(__dirname, "src", "index.js"),
+      filename:'bundle.js'
+  },
+  resolve:{
+      extensions:['.js','.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
         ]
-    },
-    plugins:[HtmlPlugin]
-}
+      },
+      {
+        test: /.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name]-[hash:8].[ext]"
+            }
+          }
+          
+        ]
+       }
+      ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
