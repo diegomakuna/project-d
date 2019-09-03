5ffc128727c42444f50f11ec79f83db96bbab687
diego.araujo <diego.araujo@netbiis.com>
2019-09-03 01:48:38 -0300

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faLinkedin, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 import "./footer.scss";
 class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <div class="footer-links">
-                  
-                    <FontAwesomeIcon icon={faLinkedin} />
-                    <FontAwesomeIcon icon={faGithubAlt} />
-
-                </div>
-              
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="footer">
+        <ul class="footer-links">
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faLinkedin} /> 
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FontAwesomeIcon icon={faGithubAlt} />
+            </a>
+          </li>
+        </ul>
+        <div class="powredby">
+            <span class="text">powred <br/>by</span>
+            <span class="year">2019</span>
+        </div>
+      </div>
+    );
+  }
 }
-export default Footer;  
+export default Footer;
