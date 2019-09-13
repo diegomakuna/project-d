7646eaefe8127ca441bd5615c37f74186317d900
diego.araujo <diego.araujo@netbiis.com>
2019-09-13 00:02:05 -0300

    layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,9 +6,16 @@ class About extends Component {
         
         return(
             <div className="section-about">
-                <div className="container-section">
-                About
-            </div>
+                <div className="about-container">
+                    <div className="since-text">SINCE 1987</div>
+                    <div className="image-about"></div>
+                    <div className="description-container">
+                        <div className="description-tilte">About Me</div>
+                        <div className="description-text">
+                        Olá, sou DIEGO ARAUJO, desevolvedor web de São Paulo, Brasil. Tenho mais de 11 anos de experiência trabalhando com diferentes cenários e tecnologias,acredito ser flexível para adquirir conhecimento e aprimorar minhas habilidades a fim de contribuir para a resolução de  problemas. Neste site você verá um pouco dos meus trabalhos e experimentos pessoais.
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
